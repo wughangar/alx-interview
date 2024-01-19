77d8e4a3b06fc1bf5ce2d0dbceeccc2aaf4706d9
wughangar <wughangar.rose@gmail.com>
2024-01-19 07:18:26 +0000

    json parse

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,21 +2,28 @@
 
 const request = require('request');
 const providedFilmId = process.argv[2];
-const filmApiUrl = `https://swapi-api.hbtn.io/api/films/${providedFilmId}`;
+const filmApiUrl = `https://swapi-api.alx-tools.com/api/films/${providedFilmId}`;
+let characterApiUrl;
 
 request(filmApiUrl, async (filmError, filmResponse, filmBody) => {
   if (filmError) {
     console.log(filmError);
+    return;
   }
-  for (const characterApiUrl of JSON.parse(filmBody).characters) {
+
+  const characters = JSON.parse(filmBody).characters;
+
+  for (characterApiUrl of characters) {
     await new Promise((resolve, reject) => {
       request(characterApiUrl, (characterError, characterResponse, characterBody) => {
         if (characterError) {
           reject(characterError);
         }
+
         console.log(JSON.parse(characterBody).name);
         resolve();
       });
     });
   }
 });
+
