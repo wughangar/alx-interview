5b0f692bb4e51be6087ccfcdbadf234c637dddb6
wughangar <wughangar.rose@gmail.com>
2024-02-15 10:08:21 +0300

    prime games

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+0. Prime Game
+"""
+
+def isWinner(x, nums):
+    def sieve(n):
+        primes = []
+        sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
+        for p in range(2, n + 1):
+            if sieve[p]:
+                primes.append(p)
+                for i in range(p * p, n + 1, p):
+                    sieve[i] = False
+        return primes
+
+    def winner(n):
+        if n == 1:
+            return "Ben"
+        primes = sieve(n)
+        if len(primes) % 2 == 0:
+            return "Ben"
+        else:
+            return "Maria"
+
+    ben_wins = 0
+    maria_wins = 0
+    for n in nums:
+        if winner(n) == "Ben":
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if ben_wins > maria_wins:
+        return "Ben"
+    elif maria_wins > ben_wins:
+        return "Maria"
+    else:
+        return None
